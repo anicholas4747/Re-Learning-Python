90384d8eaa657c81b3b54b0f18402dfe6c1c85e0
anicholas4747 <an1720@nyu.edu>
2020-06-06 15:01:39 -0400

    math module

---
+++ Mosh Tutorial/math_functions.py
@@ -0,0 +1,7 @@
+import math 
+# you can import modules from the python 3 library or other files on your computer
+# make sure not to name a file the name of a module or else python will get confused
+
+print(math.floor(2.9))
+print(math.dist([0,0],[2,2])) # euclidean distance, takes two points[] as args
+print(math.sqrt(8))
